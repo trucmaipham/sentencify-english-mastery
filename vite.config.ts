9352c25aebdb87ccc790460bfad9c99650497366
trucmaipham <74885957+trucmaipham@users.noreply.github.com>
2025-06-15 15:31:46 +0700

    Update vite.config.ts
    edited

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/sentencify-english-mastery/' : '/',
+  base: '/sentencify-english-mastery/',
   server: {
     host: "::",
     port: 8080,
